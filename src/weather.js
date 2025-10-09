e99b9ef5efb188226c77311f5cf3e91d197db98e
Phil Zhang <zhangphil@outlook.com>
2025-10-08 23:34:48 -0700

    update

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,6 @@
 export async function fetchAllData() {
   try {
-    const response = await fetch('http://35.238.45.243:6927/all');
+    const response = await fetch('snow-https://snow-scraper.duckdns.org/all');
     
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
